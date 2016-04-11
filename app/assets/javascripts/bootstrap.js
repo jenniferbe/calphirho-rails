3edf8ba19f44609ca4f7170937a26b895ba4e494
Jennifer Be <jbe@berkeley.edu>
2016-04-10 18:51:19 -0700

    dropdown jQuery

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -2366,10 +2366,10 @@ if (typeof jQuery === 'undefined') {
 
 $function() {
   // Setup drop down menu
-  $('.dropdown-toggle').dropdown();
+  $('.dropdown-toggle').dropdown()
  
   // Fix input element click problem
   $('.dropdown input, .dropdown label').click(function(e) {
-    e.stopPropagation();
+    e.stopPropagation()
   })
-}
+}(jQuery);
