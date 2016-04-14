3202d55ea7ba47fd9d0c1f83287c54ccc9ab487a
Jennifer Be <jbe@berkeley.edu>
2016-04-13 23:17:22 -0700

    added dropdown feature

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -2361,3 +2361,14 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+
+$function() {
+  // Setup drop down menu
+  $('.dropdown-toggle').dropdown();
+ 
+  // Fix input element click problem
+  $('.dropdown input, .dropdown label').click(function(e) {
+    e.stopPropagation();
+  });
+};
